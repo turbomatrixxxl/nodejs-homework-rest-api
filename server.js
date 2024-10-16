baaac797ccfd51d5b5a44fa092309872b800549f
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-10-16 14:25:50 +0300

    finished homework with all checkins

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+const uriDB = process.env.DB_URL;
+
+const connection = mongoose.connect(uriDB);
+
+connection
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server running. Use our API on port:", PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(`Server not running. Error message: ${err.message}`);
+    process.exit(1);
+  });
